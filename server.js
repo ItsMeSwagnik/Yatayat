a9ee3b0e8e1d706f8a6f150fa8d9770fd1b8bf0d
ItsMeSwagnik <swagnikganguly2004@gmail.com>
2025-11-05 12:16:28 +0530

    refactor: migrate server.js to ES module syntax

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config();
 
 const app = express();
 
